04ad013f688b0bea266701eaa21b2131e83aa575
Dragoselul <dragosbonaparte@gmail.com>
2025-07-26 13:42:33 +0000

    Finished tweaks for the pipeline

--- store/src/app/catalog/[productId]/page.tsx
+++ store/src/app/catalog/[productId]/page.tsx
@@ -11,13 +11,13 @@ import {
 import ImageCarousel from "@/components/ImageCarousel";
 import ProductDescription from "@/components/ProductDescription";
 import ClotheSizeSelector from "@/components/ClotheSizeSelector";
+ 
+type Props = {
+  params: Promise<{ productId: number }>
+}
 
-export default function ProductPage({
-  params,
-}: {
-  params: { productId: string };
-}) {
-  const { productId } = params;
+export default async function ProductPage({ params }: Props) {
+  const { productId } = await params;
   // fetch your real product here…
   const product: Product & { images: string[] } = {
     id: Number(productId),
@@ -27,7 +27,7 @@ export default function ProductPage({
       ["Details", `More details about product ${productId}`],
       ["Specifications", `Specifications for product ${productId}`],
     ]),
-    price: 19.99 + Number(productId),
+    price: 19.99,
     images: [
       "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?auto=format&fit=crop&w=800",
       "https://images.unsplash.com/photo-1752771433743-47a49376fb63?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
